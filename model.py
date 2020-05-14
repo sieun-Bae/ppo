db3bbf01f84166b61051a600629cfae8a0d3c4b8
sieun-bae <qotldms130@gmail.com>
2020-05-14 14:58:44 +0900

    lstm added

--- model.py
+++ model.py
@@ -23,23 +23,26 @@ class PPO(nn.Module):
 		self.data = []
 
 		self.fc1 = nn.Linear(8, 256)
-		self.lstm = nn.LSTM(256, 64)
-		self.fc_pi = nn.Linear(64, 4)
-		self.fc_v = nn.Linear(64, 1)
+		self.fc2 = nn.Linear(256, 64)
+		self.lstm = nn.LSTM(64, 32)
+		self.fc_pi = nn.Linear(32, 4)
+		self.fc_v = nn.Linear(32, 1)
 		self.optimizer = optim.Adam(self.parameters(), lr=0.0005)
 
 	#policy network	
-	def pi(self, x, softmax_dim = 0):
+	def pi(self, x, hidden):
 		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
 		x = x.view(-1, 1, 64)
 		x, lstm_hidden = self.lstm(x, hidden)
 		x = self.fc_pi(x)
-		prob = F.softmax(x, dim=softmax_dim)
-		return prob
+		prob = F.softmax(x, dim=2)
+		return prob, lstm_hidden
 
 	#value network
-	def v(self, x):
+	def v(self, x, hidden):
 		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
 		x = x.view(-1, 1, 64)
 		x, lstm_hidden = self.lstm(x, hidden)
 		v = self.fc_v(x)
@@ -51,7 +54,7 @@ class PPO(nn.Module):
 	def make_batch(self):
 		s_lst, a_lst, r_lst, s_prime_lst, prob_a_lst, h_in_lst, h_out_lst, done_lst = [], [], [], [], [], [], [], []
 		for transition in self.data:
-			s, a, r, s_prime, prob_a, done = transition
+			s, a, r, s_prime, prob_a, h_in, h_out, done = transition
 
 			s_lst.append(s)
 			a_lst.append([a])
@@ -77,10 +80,10 @@ class PPO(nn.Module):
 
 		for i in range(K_epoch):
 			v_prime = self.v(s_prime, second_hidden).squeeze(1)
-
-			td_target = r+gamma*self.v(s_prime)*done_mask
-			delta = td_target-self.v(s)
-			delta=delta.detach().numpy()
+			td_target = r + gamma*v_prime*done_mask
+			v_s = self.v(s, first_hidden).squeeze(1)
+			delta = td_target - v_s
+			delta = delta.detach().numpy()
 
 			advantage_lst = []
 			advantage = 0.0
@@ -90,35 +93,44 @@ class PPO(nn.Module):
 			advantage_lst.reverse()
 			advantage = torch.tensor(advantage_lst, dtype=torch.float)
 
-			pi = self.pi(s, softmax_dim = 1)
-			pi_a = pi.gather(1, a)
+			pi, _ = self.pi(s, first_hidden)
+			pi_a = pi.squeeze(1).gather(1, a)
 
 			ratio = torch.exp(torch.log(pi_a)-torch.log(prob_a))
 
 			surr1 = ratio*advantage
 			surr2 = torch.clamp(ratio, 1-eps_clip, 1+eps_clip)*advantage
-			loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(td_target.detach(), self.v(s))
+			loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(v_s, td_target.detach())
 
 			self.optimizer.zero_grad()
-			loss.mean().backward()
+			loss.mean().backward(retain_graph = True)
 			self.optimizer.step()
+
 def main():
 	env = gym.make('LunarLander-v2')
 	model = PPO()
+
+	f = open("./train_log.txt", "a")
+
 	score = 0.0
 	print_interval = 20
 
 	for n_epi in range(200000):
+		h_out = (torch.zeros([1, 1, 32], dtype=torch.float), torch.zeros([1, 1, 32], dtype=torch.float))
 		s = env.reset()
 		done = False
 		while not done:
 			for t in range(T_horizon):
-				prob = model.pi(torch.from_numpy(s).float())
+				h_in = h_out
+				prob, h_out = model.pi(torch.from_numpy(s).float(), h_in)
+				prob = prob.view(-1)
+
 				m = Categorical(prob)
 				a = m.sample().item()
-				env.render()
+				
 				s_prime, r, done, info = env.step(a)
-				model.put_data((s, a, r/100.0, s_prime, prob[a].item(), done))
+				
+				model.put_data((s, a, r/100.0, s_prime, prob[a].item(), h_in, h_out, done))
 				s = s_prime
 
 				score += r
@@ -127,10 +139,13 @@ def main():
 			model.train_net()
 			
 		if n_epi%print_interval == 0 and n_epi!=0:
-			print("# of episode: {}, avg score: {:.1f}".format(n_epi, score/print_interval))
+			data ="# of episode: {}, avg score: {:.1f}".format(n_epi, score/print_interval) 
+			print(data)
+			f.write(data)
 			score = 0.0
 
 	env.close()
+	f.close()	
 
 if __name__ == '__main__':
 	main()
